e828d45b542a79782bf0f1a23ff2e10fa53e286d
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-06-28 23:29:55 +0530

    fix doc for :engine key in sample config

--- sample.project.clj
+++ sample.project.clj
@@ -6,23 +6,25 @@
   ;; Project level usage of the lein-servlet plugin is recommended
   :plugins [[lein-servlet "0.1.0"]]
   ;; Configuration for the :lein-servlet plugin (required)
-  :servlet {;;; (required) engine adapters should be specified here
+  :servlet {;;; (required) At most one engine adapter should be specified here
             :deps [[lein-servlet/adapter-jetty "0.1.0"]]
-            ;;; (optional) map of engine attributes
-            :config {;; (optional) unless specified, first available is used
+            ;;; (optional) Map of engine attributes
+            :config {;; (optional) Unless specified, first available engine in
+                     ;; classpath is used. Useful in rare cases when more than
+                     ;; one engine is on classpath; to disambiguate.
                      :engine :jetty
-                     ;; (optional) default host is "localhost"
+                     ;; (optional) Default host is "localhost"
                      :host   "0.0.0.0"
-                     ;; (optional) default port is 3000
+                     ;; (optional) Default port is 3000
                      :port   3000
-                     ;; (optional) default is "<target-path>/<eng-name>.<port>"
+                     ;; (optional) Default is "<target-path>/<eng-name>.<port>"
                      :tmpdir "target/engine"
                      ;; (optional) SSL will be configured only when specified
                      :ssl    {;; (optional) default is 3443
                               :port 3443
-                              ;; (optional) default is "$HOME/.keystore"
+                              ;; (optional) Default is "$HOME/.keystore"
                               :keystore-path       "/home/joe/.keystore"
-                              ;; (required) password for your keystore
+                              ;; (required) Password for your keystore
                               :keystore-password   "s3cr3t"
                               ;; FIXME
                               :keymanager-password ""
@@ -32,25 +34,25 @@
                               :truststore-password ""}
                      ;; (optional) TODO
                      :datasource {}}
-            ;;; (required) non-empty map of context-path to webapp-config
-            :webapps {;; (required) context-path - turns into '/app1'
+            ;;; (required) Non-empty map of context-path to webapp-config
+            :webapps {;; (required) Context-path - turns into '/app1'
                       :app1
-                      {;; map of URL-patterns to servlet classes
-                       ;; every webapp must have either :classes or :web-xml
+                      {;; Map of URL-patterns to servlet classes.
+                       ;; Every webapp must have either :classes or :web-xml
                        :classes {"/*"      com.myapp.WebappServlet
                                  "/fr/*"   [com.myapp.WebappServlet "fr"]
                                  "/it/*"   [com.myapp.WebappServlet "it"]
                                  "/auth/*" com.myapp.AuthServlet}
-                       ;; (required) directory location for public resources
+                       ;; (required) Directory location for public resources
                        :public  "public"}}
-                     {;; (required) context-path of the webapp
+                     {;; (required) Context-path of the webapp
                       "/app2"
-                      {;; path to the `web.xml` file
-                       ;; every webapp must have either :classes or :web-xml
+                      {;; Path to the `web.xml` file
+                       ;; Every webapp must have either :classes or :web-xml
                        :web-xml "public/WEB-INF/web.xml"
-                       ;; (required) directory location for public resources
+                       ;; (required) Directory location for public resources
                        :public  "public"
-                       ;;; (optional) per webapp engine-specific config
+                       ;;; (optional) Per webapp engine-specific config
                        :config {:parent-loader-priority? true
                                 :system-classes []
                                 :server-classes []}}}})
