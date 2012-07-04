8871a9dff93f321a2cae0835f51b198586678c74
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-07-04 22:15:21 +0530

    update Jetty 7, Jetty 8 and Tomcat 7 deps for v0.2.0

--- adapter-jetty7/project.clj
+++ adapter-jetty7/project.clj
@@ -1,12 +1,12 @@
-(defproject lein-servlet/adapter-jetty7 "0.1.0"
+(defproject lein-servlet/adapter-jetty7 "0.2.0"
   :description "Jetty 7 adapter for lein-servlet"
   :url "https://github.com/kumarshantanu/lein-servlet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[org.eclipse.jetty/jetty-server  "7.6.3.v20120416"]
-                 [org.eclipse.jetty/jetty-servlet "7.6.3.v20120416"]
-                 [org.eclipse.jetty/jetty-webapp  "7.6.3.v20120416"]
-                 [org.eclipse.jetty/jetty-jsp     "7.6.3.v20120416"]
-                 [org.eclipse.jetty/jetty-jndi    "7.6.3.v20120416"]])
+  :dependencies [[org.eclipse.jetty/jetty-server  "7.6.4.v20120524"]
+                 [org.eclipse.jetty/jetty-servlet "7.6.4.v20120524"]
+                 [org.eclipse.jetty/jetty-webapp  "7.6.4.v20120524"]
+                 [org.eclipse.jetty/jetty-jsp     "7.6.4.v20120524"]
+                 [org.eclipse.jetty/jetty-jndi    "7.6.4.v20120524"]])
 
--- adapter-jetty8/project.clj
+++ adapter-jetty8/project.clj
@@ -1,15 +1,15 @@
-(defproject lein-servlet/adapter-jetty8 "0.1.0"
+(defproject lein-servlet/adapter-jetty8 "0.2.0"
   :description "Jetty 8 adapter for lein-servlet"
   :url "https://github.com/kumarshantanu/lein-servlet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[org.eclipse.jetty/jetty-server  "8.1.3.v20120416"]
-                 [org.eclipse.jetty/jetty-servlet "8.1.3.v20120416"]
-                 [org.eclipse.jetty/jetty-webapp  "8.1.3.v20120416"]
-                 [org.eclipse.jetty/jetty-jsp     "8.1.3.v20120416"]
-                 [org.eclipse.jetty/jetty-jndi    "8.1.3.v20120416"]
-                 [lein-servlet/adapter-jetty7     "0.1.0"
+  :dependencies [[org.eclipse.jetty/jetty-server  "8.1.4.v20120524"]
+                 [org.eclipse.jetty/jetty-servlet "8.1.4.v20120524"]
+                 [org.eclipse.jetty/jetty-webapp  "8.1.4.v20120524"]
+                 [org.eclipse.jetty/jetty-jsp     "8.1.4.v20120524"]
+                 [org.eclipse.jetty/jetty-jndi    "8.1.4.v20120524"]
+                 [lein-servlet/adapter-jetty7     "0.2.0"
                   :exclusions [org.eclipse.jetty/jetty-server
                                org.eclipse.jetty/jetty-servlet
                                org.eclipse.jetty/jetty-webapp
