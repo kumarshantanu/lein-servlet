628c97cce4d5cc80a8b1d66a02d327b1bae8d51c
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-07-16 20:49:16 +0530

    bump Tomcat version to 7.0.29

--- adapter-tomcat7/project.clj
+++ adapter-tomcat7/project.clj
@@ -7,11 +7,11 @@
   :dependencies
   [;; see http://people.apache.org/~markt/presentations/2010-11-04-Embedding-Tomcat.pdf
    ;; required
-   [org.apache.tomcat.embed/tomcat-embed-core          "7.0.28"]
-   [org.apache.tomcat.embed/tomcat-embed-logging-juli  "7.0.28"]
-   [org.apache.tomcat.embed/tomcat-embed-logging-log4j "7.0.28"]
+   [org.apache.tomcat.embed/tomcat-embed-core          "7.0.29"]
+   [org.apache.tomcat.embed/tomcat-embed-logging-juli  "7.0.29"]
+   [org.apache.tomcat.embed/tomcat-embed-logging-log4j "7.0.29"]
    ;; optional, Connection pooling
-   [org.apache.tomcat/tomcat-dbcp               "7.0.28"]
+   [org.apache.tomcat/tomcat-dbcp               "7.0.29"]
    ;; optional, JSP support
-   [org.apache.tomcat.embed/tomcat-embed-jasper "7.0.28"]
+   [org.apache.tomcat.embed/tomcat-embed-jasper "7.0.29"]
    [org.eclipse.jdt.core.compiler/ecj           "3.7.2"]])
