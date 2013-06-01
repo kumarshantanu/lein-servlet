f3a68c46d15e34398190421a6d6ef8268052ccc8
Shantanu Kumar <kumar.shantanu@gmail.com>
2013-06-02 03:15:52 +0530

    update Tomcat 7 Maven coords

--- adapter-tomcat7/project.clj
+++ adapter-tomcat7/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-servlet/adapter-tomcat7 "0.2.0"
+(defproject lein-servlet/adapter-tomcat7 "0.3.0"
   :description "Tomcat 7 adapter for lein-servlet"
   :url "https://github.com/kumarshantanu/lein-servlet"
   :license {:name "Eclipse Public License"
@@ -7,11 +7,11 @@
   :dependencies
   [;; see http://people.apache.org/~markt/presentations/2010-11-04-Embedding-Tomcat.pdf
    ;; required
-   [org.apache.tomcat.embed/tomcat-embed-core          "7.0.29"]
-   [org.apache.tomcat.embed/tomcat-embed-logging-juli  "7.0.29"]
-   [org.apache.tomcat.embed/tomcat-embed-logging-log4j "7.0.29"]
+   [org.apache.tomcat.embed/tomcat-embed-core          "7.0.40"]
+   [org.apache.tomcat.embed/tomcat-embed-logging-juli  "7.0.40"]
+   [org.apache.tomcat.embed/tomcat-embed-logging-log4j "7.0.40"]
    ;; optional, Connection pooling
-   [org.apache.tomcat/tomcat-dbcp               "7.0.29"]
+   [org.apache.tomcat/tomcat-dbcp               "7.0.40"]
    ;; optional, JSP support
-   [org.apache.tomcat.embed/tomcat-embed-jasper "7.0.29"]
-   [org.eclipse.jdt.core.compiler/ecj           "3.7.2"]])
+   [org.apache.tomcat.embed/tomcat-embed-jasper "7.0.40"]
+   [org.eclipse.jdt.core.compiler/ecj           "4.2.2"]])
