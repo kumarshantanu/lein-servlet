ff1bb14f98499040ae957bbf4b7201dc611f2174
Shantanu Kumar <kumar.shantanu@gmail.com>
2013-06-02 03:15:04 +0530

    update Jetty 7 Maven coords

--- adapter-jetty7/project.clj
+++ adapter-jetty7/project.clj
@@ -1,12 +1,11 @@
-(defproject lein-servlet/adapter-jetty7 "0.2.0"
+(defproject lein-servlet/adapter-jetty7 "0.3.0"
   :description "Jetty 7 adapter for lein-servlet"
   :url "https://github.com/kumarshantanu/lein-servlet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[org.eclipse.jetty/jetty-server  "7.6.4.v20120524"]
-                 [org.eclipse.jetty/jetty-servlet "7.6.4.v20120524"]
-                 [org.eclipse.jetty/jetty-webapp  "7.6.4.v20120524"]
-                 [org.eclipse.jetty/jetty-jsp     "7.6.4.v20120524"]
-                 [org.eclipse.jetty/jetty-jndi    "7.6.4.v20120524"]])
-
+  :dependencies [[org.eclipse.jetty/jetty-server  "7.6.11.v20130520"]
+                 [org.eclipse.jetty/jetty-servlet "7.6.11.v20130520"]
+                 [org.eclipse.jetty/jetty-webapp  "7.6.11.v20130520"]
+                 [org.eclipse.jetty/jetty-jsp     "7.6.11.v20130520"]
+                 [org.eclipse.jetty/jetty-jndi    "7.6.11.v20130520"]])
