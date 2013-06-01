4678c2ce6821ef915ce5d092f421fb35c788d6de
Shantanu Kumar <kumar.shantanu@gmail.com>
2013-06-02 03:19:28 +0530

    bump template version to 0.3.0

--- lein-template/project.clj
+++ lein-template/project.clj
@@ -1,7 +1,7 @@
-(defproject lein-servlet/lein-template "0.2.0"
+(defproject lein-servlet/lein-template "0.3.0"
   :description "Templates for lein-servlet"
   :url "https://github.com/kumarshantanu/lein-servlet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :warn-on-reflection true)
