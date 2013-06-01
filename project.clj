b7ea4236d6cd343ea45144c6b4169bd4c9c44a85
Shantanu Kumar <kumar.shantanu@gmail.com>
2013-06-02 03:20:35 +0530

    include adapter-jetty9 in parent project

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject lein-servlet/parent "0.0.0"
   :description "Housekeeping project for lein-servlet"
   :url "https://github.com/kumarshantanu/lein-servlet"
-  :plugins [[lein-sub "0.2.0"]]
+  :plugins [[lein-sub "0.2.4"]]
   :sub ["adapter-jetty7"
         "adapter-jetty8"
+        "adapter-jetty9"
         "adapter-tomcat7"
         "lein-template"
         "plugin"]
