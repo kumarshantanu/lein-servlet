5ce0cd67e9f9b5aeb43589063b414f16e0ae23c0
Shantanu Kumar <kumar.shantanu@gmail.com>
2014-03-22 04:19:31 +0530

    add Tomcat 8 adapter

---
+++ adapter-tomcat8/project.clj
@@ -0,0 +1,24 @@
+(defproject lein-servlet/adapter-tomcat8 "0.4.0"
+  :description "Tomcat 8 adapter for lein-servlet"
+  :url "https://github.com/kumarshantanu/lein-servlet"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true
+  :dependencies
+  [;; see http://people.apache.org/~markt/presentations/2010-11-04-Embedding-Tomcat.pdf
+   ;; required
+   [org.apache.tomcat.embed/tomcat-embed-core          "8.0.3"]
+   [org.apache.tomcat.embed/tomcat-embed-logging-juli  "8.0.3"]
+   [org.apache.tomcat.embed/tomcat-embed-logging-log4j "8.0.3"]
+   ;; optional, Connection pooling
+   [org.apache.tomcat/tomcat-dbcp               "8.0.3"]
+   ;; optional, JSP support
+   [org.apache.tomcat.embed/tomcat-embed-jasper "8.0.3"]
+   [org.eclipse.jdt.core.compiler/ecj           "4.3.1"]
+   [lein-servlet/adapter-tomcat7 "0.4.0"
+    :exclusions [org.apache.tomcat.embed/tomcat-embed-core
+                 org.apache.tomcat.embed/tomcat-embed-logging-juli
+                 org.apache.tomcat.embed/tomcat-embed-logging-log4j
+                 org.apache.tomcat/tomcat-dbcp
+                 org.apache.tomcat.embed/tomcat-embed-jasper
+                 org.eclipse.jdt.core.compiler/ecj]]])
